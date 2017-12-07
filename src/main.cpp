cc78c01ef3fb9e737745e3314ae18a58c9175f24
M <matttawil@utexas.edu>
2017-12-07 10:23:29 -0600

    hotfix

--- src/main.cpp
+++ src/main.cpp
@@ -597,8 +597,6 @@ static const int DevRewardStopBlock = 235000;
 
 int64 GetFounderRewardPerAddr(int nHeight)
 {
-	if ((nHeight < DevRewardStartBlock) || (nHeight > DevRewardStopBlock))
-		return 0;
 	int64 Subsidy = GetBlockValue(nHeight, 0);
 	int64 Reward = 38 * COIN;
 	int64 RewardPerAddr = Reward / 2;
@@ -5184,7 +5182,7 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn)
 			 // And give it to the Developers, 19 per address
 			 txNew.vout.push_back(CTxOut(FounderRewardPerAddr, CScript(FOUNDER_1_SCRIPT.begin(), FOUNDER_1_SCRIPT.end())));
 			 txNew.vout.push_back(CTxOut(FounderRewardPerAddr, CScript(FOUNDER_2_SCRIPT.begin(), FOUNDER_2_SCRIPT.end())));
-			 printf("Push back: %d to " FounderRewardPerAddr);
+			 printf("Push back: %d to ", FounderRewardPerAddr);
             		 FOUNDER_1_SCRIPT.print();
             		 FOUNDER_1_SCRIPT.print();
 			 //txNew.vout.push_back(CTxOut(FounderRewardPerAddr, CScript(FOUNDER_3_SCRIPT.begin(), FOUNDER_3_SCRIPT.end())));
