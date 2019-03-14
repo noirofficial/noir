0ad12aff3aa48624ed0aa53f799cc309695fdf84
flo071 <flo071@gmx.at>
2019-03-14 20:56:17 +0100

    clean up splashscreen code

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -28,163 +28,23 @@
 
 SplashScreen::SplashScreen(const QPixmap &pixmap, Qt::WindowFlags f) : QSplashScreen(pixmap, f)
 {
-    // set reference point, paddings
-    int paddingLeftCol2         = 232;
-    int paddingTopCol2          = 140;
-    int line1 = 0;
-    int line2 = 13;
-    int line3 = 26;
-    int line4 = 39;
 
     float fontFactor            = 1.5;
-
-    // define text to place
-    //QString titleText       = QString("Noir Core"); // cut of testnet, place it as single object further down
-    //QString versionText     = QString("Version %1 ").arg(QString::fromStdString(FormatFullVersion()));
-    //QString copyrightText1   = QChar(0xA9)+QString(" 2009-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Bitcoin developers"));
-    //QString copyrightText2   = QChar(0xA9)+QString(" 2011-%1 ").arg(COPYRIGHT_YEAR) + QString(tr("The Litecoin developers"));
-    //QString copyrightText3   = QChar(0xA9)+QString(" 2014 ") + QString(tr("The ZCoin developers"));
-
     QString font            = "Yu Gothic UI Light";
-
-    // load the bitmap for writing some text over it
     QPixmap newPixmap;
-//    if(GetBoolArg("-testnet")) {
-//        newPixmap     = QPixmap(":/images/splash_testnet");
-//    }
-//    else {
     newPixmap     = QPixmap(":/images/splash");
-//    }
+
 
     QPainter pixPaint(&newPixmap);
     pixPaint.setPen(QColor(70,70,70));
 
     pixPaint.setFont(QFont(font, 12*fontFactor));
-    //pixPaint.drawText(QRect(0, paddingTopCol2+line1, 358, 299 ), Qt::AlignHCenter,titleText);
-    //pixPaint.drawText(QRect(0, paddingTopCol2+line3, 358, 299 ), Qt::AlignHCenter, versionText);
-
-    // draw copyright stuff
     pixPaint.setFont(QFont(font, 9*fontFactor));
-    //pixPaint.drawText(paddingLeftCol2,paddingTopCol2+line1,copyrightText1);
-    //pixPaint.drawText(paddingLeftCol2,paddingTopCol2+line2,copyrightText2);
-    //pixPaint.drawText(paddingLeftCol2,paddingTopCol2+line3,copyrightText3);
 
     pixPaint.end();
 
     this->setPixmap(newPixmap);
 }
-
-
-//SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) : QWidget(0, f), curAlignment(0)
-//{
-//    // set reference point, paddings
-//    int paddingRight            = 50;
-//    int paddingTop              = 50;
-//    int titleVersionVSpace      = 17;
-//    int titleCopyrightVSpace    = 40;
-//
-//    float fontFactor            = 1.0;
-//    float devicePixelRatio      = 1.0;
-//#if QT_VERSION > 0x050100
-//    devicePixelRatio = ((QGuiApplication*)QCoreApplication::instance())->devicePixelRatio();
-//#endif
-//
-//    // define text to place
-//    QString titleText       = tr(PACKAGE_NAME);
-//    QString versionText     = QString("Version %1").arg(QString::fromStdString(FormatFullVersion()));
-//    QString copyrightText   = QString::fromUtf8(CopyrightHolders(strprintf("\xc2\xA9 %u-%u ", 2009, COPYRIGHT_YEAR)).c_str());
-//    QString titleAddText    = networkStyle->getTitleAddText();
-//
-//    QString font            = QApplication::font().toString();
-//
-//    // create a bitmap according to device pixelratio
-//    QSize splashSize(480*devicePixelRatio,320*devicePixelRatio);
-//    pixmap = QPixmap(splashSize);
-//
-//#if QT_VERSION > 0x050100
-//    // change to HiDPI if it makes sense
-//    pixmap.setDevicePixelRatio(devicePixelRatio);
-//#endif
-//
-//    QPainter pixPaint(&pixmap);
-//    pixPaint.setPen(QColor(100,100,100));
-//
-//    // draw a slightly radial gradient
-//    QRadialGradient gradient(QPoint(0,0), splashSize.width()/devicePixelRatio);
-//    gradient.setColorAt(0, Qt::white);
-//    gradient.setColorAt(1, QColor(247,247,247));
-//    QRect rGradient(QPoint(0,0), splashSize);
-//    pixPaint.fillRect(rGradient, gradient);
-//
-//    // draw the bitcoin icon, expected size of PNG: 1024x1024
-//    QRect rectIcon(QPoint(-150,-122), QSize(430,430));
-//
-//    const QSize requiredSize(1024,1024);
-//    QPixmap icon(networkStyle->getAppIcon().pixmap(requiredSize));
-//
-//    pixPaint.drawPixmap(rectIcon, icon);
-//
-//    // check font size and drawing with
-//    pixPaint.setFont(QFont(font, 33*fontFactor));
-//    QFontMetrics fm = pixPaint.fontMetrics();
-//    int titleTextWidth = fm.width(titleText);
-//    if (titleTextWidth > 176) {
-//        fontFactor = fontFactor * 176 / titleTextWidth;
-//    }
-//
-//    pixPaint.setFont(QFont(font, 33*fontFactor));
-//    fm = pixPaint.fontMetrics();
-//    titleTextWidth  = fm.width(titleText);
-//    pixPaint.drawText(pixmap.width()/devicePixelRatio-titleTextWidth-paddingRight,paddingTop,titleText);
-//
-//    pixPaint.setFont(QFont(font, 15*fontFactor));
-//
-//    // if the version string is to long, reduce size
-//    fm = pixPaint.fontMetrics();
-//    int versionTextWidth  = fm.width(versionText);
-//    if(versionTextWidth > titleTextWidth+paddingRight-10) {
-//        pixPaint.setFont(QFont(font, 10*fontFactor));
-//        titleVersionVSpace -= 5;
-//    }
-//    pixPaint.drawText(pixmap.width()/devicePixelRatio-titleTextWidth-paddingRight+2,paddingTop+titleVersionVSpace,versionText);
-//
-//    // draw copyright stuff
-//    {
-//        pixPaint.setFont(QFont(font, 10*fontFactor));
-//        const int x = pixmap.width()/devicePixelRatio-titleTextWidth-paddingRight;
-//        const int y = paddingTop+titleCopyrightVSpace;
-//        QRect copyrightRect(x, y, pixmap.width() - x - paddingRight, pixmap.height() - y);
-//        pixPaint.drawText(copyrightRect, Qt::AlignLeft | Qt::AlignTop | Qt::TextWordWrap, copyrightText);
-//    }
-//
-//    // draw additional text if special network
-//    if(!titleAddText.isEmpty()) {
-//        QFont boldFont = QFont(font, 10*fontFactor);
-//        boldFont.setWeight(QFont::Bold);
-//        pixPaint.setFont(boldFont);
-//        fm = pixPaint.fontMetrics();
-//        int titleAddTextWidth  = fm.width(titleAddText);
-//        pixPaint.drawText(pixmap.width()/devicePixelRatio-titleAddTextWidth-10,15,titleAddText);
-//    }
-//
-//    pixPaint.end();
-//
-//    // Set window title
-//    setWindowTitle(titleText + " " + titleAddText);
-//
-//    // Resize window and move to center of desktop, disallow resizing
-//    QRect r(QPoint(), QSize(pixmap.size().width()/devicePixelRatio,pixmap.size().height()/devicePixelRatio));
-//    resize(r.size());
-//    setFixedSize(r.size());
-//    move(QApplication::desktop()->screenGeometry().center() - r.center());
-//
-//    subscribeToCoreSignals();
-//}
-//
-//SplashScreen::~SplashScreen()
-//{
-//    unsubscribeFromCoreSignals();
-//}
 
 void SplashScreen::slotFinish(QWidget *mainWin)
 {
