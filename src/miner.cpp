feaf0981ca69cf7f20edcbc69fc6fc3bf39848cf
flo071 <floh071@gmail.com>
2019-10-17 20:51:35 +0200

    fix "start staking when last PoW block was mined"

--- src/miner.cpp
+++ src/miner.cpp
@@ -1343,10 +1343,10 @@ void ThreadStakeMiner(CWallet *pwallet, const CChainParams& chainparams)
     
     bool fTestNet = (Params().NetworkIDString() == CBaseChainParams::TESTNET);
     bool fTryToSync = true;
-    CBlockIndex* pindexPrev = chainActive.Tip();
-    const int nHeight = pindexPrev->nHeight + 1;
     while (true)
     {
+        CBlockIndex* pindexPrev = chainActive.Tip();
+        const int nHeight = pindexPrev->nHeight + 1;
         if (nHeight >= Params().GetConsensus().nLastPOWBlock)
         {
             while (pwallet->IsLocked())
