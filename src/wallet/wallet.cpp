5d0c3b36ff867899704dcbd205055565556bfa18
Pellkopf <31163021+Pellkopf@users.noreply.github.com>
2020-01-31 22:22:25 +0100

    Test cache

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -745,7 +745,7 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
     if (setCoins.empty())
         return false;
 
-    /*if (stakeCache.size() > setCoins.size() + 100){
+    if (stakeCache.size() > setCoins.size() + 100){
         //Determining if the cache is still valid is harder than just clearing it when it gets too big, so instead just clear it
         //when it has more than 100 entries more than the actual setCoins.
         stakeCache.clear();
@@ -759,7 +759,7 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
             CacheKernel(stakeCache, prevoutStake, pindexPrev); //this will do a 2 disk loads per op
         }
 
-    }*/
+    }
 
     int64_t nCredit = 0;
     CScript scriptPubKeyKernel;
